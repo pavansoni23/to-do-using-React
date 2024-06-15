cb7ca11ac3672e78479a8fb5716f37a38569a83c
pavansoni23 <123145706+pavansoni23@users.noreply.github.com>
2024-06-15 16:05:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
-// import Counter from './Counter';
-// import LikeButton from './LikeButton.jsx';
-// import LudoBoard from './Ludo/LudoBoard';
 import TodoList from './To-Do/TodoList';
 
 function App() {
   return (
     <>
-      {/* <Counter /> */}
-      {/* <LikeButton /> */}
-      {/* <LudoBoard /> */}
-
       <TodoList />
     </>
   );
